5835def6616c34d6ccb9019977763abff8e4c84d
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-12-18 17:40:22 +0100

    change webpack config 2

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,30 @@ module.exports = {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 	},
-	// Другие опции конфигурации webpack...
 	resolve: {
 		fallback: { stream: require.resolve('stream-browserify') },
 	},
 	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			// Добавлено для обработки стилей
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			// Добавлено для обработки изображений
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			// Добавлено для обработки других типов файлов, если это необходимо
+		],
+	},
 };
