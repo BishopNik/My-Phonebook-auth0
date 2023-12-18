addb129ea2dcdb534a1de02a23c21ac547bc7566
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-12-18 19:41:08 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,6 @@
 /** @format */
 
-const path = require('path');
-
 module.exports = {
-	entry: 'index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
 	mode: 'development',
 	module: {
 		rules: [
